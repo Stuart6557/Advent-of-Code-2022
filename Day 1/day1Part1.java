91bad8afbdb2eff799fe8299cd584dd94f24aa25
Stuart6557 <vivianliu532@gmail.com>
2022-12-01 22:14:46 -0800

    improved style

--- Day 1/day1Part1.java
+++ Day 1/day1Part1.java
@@ -1,15 +1,34 @@
+/*
+ * This file is for Advent of Code Day 1
+ */
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;;
 
+/**
+ * This class is my solution for Day 1 Part 1
+ */
 public class day1Part1 {
+    /* Constants (Magic numbers) */
+    private static final String INPUT_FILE = "day1Input.txt";
+    private static final String ELF_SEPARATOR = "";
+
+    /**
+     * Calculates and prints how many Calories are being carried by the Elf
+     * carrying the most Calories
+     * 
+     * @param args not used
+     * @throws FileNotFoundException if INPUT_FILE doesn't exist
+     */
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("day1Input.txt"));
+        Scanner input = new Scanner(new File(INPUT_FILE));
         int currElfCalories = 0;
         int maxElfCalories = 0;
+
         while(input.hasNext()) {
             String nextVal = input.nextLine();
-            if(nextVal.equals("")) {
+            if(nextVal.equals(ELF_SEPARATOR)) {
                 if(currElfCalories > maxElfCalories) {
                     maxElfCalories = currElfCalories;
                 }
@@ -19,6 +38,7 @@ public class day1Part1 {
                 currElfCalories += numCalories;
             }
         }
+        
         System.out.println(maxElfCalories);
     }
 }
--- Day 1/day1Part2.java
+++ Day 1/day1Part2.java
@@ -1,17 +1,36 @@
+/*
+ * This file is for Advent of Code Day 1
+ */
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;;
 
+/**
+ * This class is my solution for Day 1 Part 2
+ */
 public class day1Part2 {
+    /* Constants (Magic numbers) */
+    private static final String INPUT_FILE = "day1Input.txt";
+    private static final String ELF_SEPARATOR = "";
+
+    /**
+     * Calculates and prints the total Calories carried by the top three Elves
+     * carrying the most Calories.
+     * 
+     * @param args not used
+     * @throws FileNotFoundException if INPUT_FILE doesn't exist
+     */
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("day1Input.txt"));
+        Scanner input = new Scanner(new File(INPUT_FILE));
         int currElfCalories = 0;
         int maxElfCalories = 0;
         int secondMostElfCalories = 0;
         int thirdMostElfCalories = 0;
+        
         while(input.hasNext()) {
             String nextVal = input.nextLine();
-            if(nextVal.equals("")) {
+            if(nextVal.equals(ELF_SEPARATOR)) {
                 if(currElfCalories > maxElfCalories) {
                     thirdMostElfCalories = secondMostElfCalories;
                     secondMostElfCalories = maxElfCalories;
@@ -28,6 +47,7 @@ public class day1Part2 {
                 currElfCalories += numCalories;
             }
         }
+
         System.out.println(maxElfCalories + secondMostElfCalories + thirdMostElfCalories);
     }
 }
