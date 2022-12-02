2961f144f472fdc4651633b5f57d196f0b7f1fbd
Stuart6557 <vivianliu532@gmail.com>
2022-12-01 22:21:58 -0800

    created templates for future problems

---
+++ Day 3/day3Part1.java
@@ -0,0 +1,24 @@
+/*
+ * This file is for Advent of Code Day 3
+ */
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+/**
+ * This class is my solution for Day 3 Part 1
+ */
+public class day3Part1 {
+    /* Constants (Magic numbers) */
+    private static final String INPUT_FILE = "";
+
+    /**
+     * 
+     * @param args not used
+     * @throws FileNotFoundException if INPUT_FILE doesn't exist
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
---
+++ Day 3/day3Part2.java
@@ -0,0 +1,24 @@
+/*
+ * This file is for Advent of Code Day 3
+ */
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+/**
+ * This class is my solution for Day 3 Part 2
+ */
+public class day3Part2 {
+    /* Constants (Magic numbers) */
+    private static final String INPUT_FILE = "";
+
+    /**
+     * 
+     * @param args not used
+     * @throws FileNotFoundException if INPUT_FILE doesn't exist
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
