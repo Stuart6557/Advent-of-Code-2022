cb159168f9b70e83094114cf3b3ab1f8e533ed76
Stuart6557 <vivianliu532@gmail.com>
2022-12-09 05:22:53 -0800

    created templates for day 10

---
+++ Day 10/day10Part1.java
@@ -0,0 +1,25 @@
+/*
+ * This file is for Advent of Code Day 10
+ */
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+/**
+ * This class is my solution for Day 10 Part 1
+ */
+public class day10Part1 {
+    /* Constants (Magic numbers) */
+    private static final String INPUT_FILE = "day10Input.txt";
+
+    /**
+     * Description
+     * 
+     * @param args not used
+     * @throws FileNotFoundException if INPUT_FILE doesn't exist
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(INPUT_FILE));
+    }
+}
